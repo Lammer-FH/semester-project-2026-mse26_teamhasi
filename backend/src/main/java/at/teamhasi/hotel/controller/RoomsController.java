54063759bc8dfe897699e4b0c6e53ad873a62167
Patrick <patrick.roessler@edu.fh-joanneum.at>
2026-05-13 20:23:04 +0200

    produces JSON

--- backend/src/main/java/at/teamhasi/hotel/controller/RoomsController.java
+++ backend/src/main/java/at/teamhasi/hotel/controller/RoomsController.java
@@ -3,6 +3,7 @@ package at.teamhasi.hotel.controller;
 import at.teamhasi.hotel.entities.RoomEntity;
 import at.teamhasi.hotel.services.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class RoomsController {
 
     private final RoomService roomService;
 
-    @GetMapping
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RoomEntity>> getAllRooms() {
         return ResponseEntity.ok(roomService.getAllRooms());
     }
