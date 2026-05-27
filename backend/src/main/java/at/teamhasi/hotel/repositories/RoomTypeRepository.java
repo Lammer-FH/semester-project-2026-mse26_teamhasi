a9efde7ef6854d61fcd97455060d1efa22b6d6e6
Patrick <patrick.roessler@edu.fh-joanneum.at>
2026-05-27 21:25:41 +0200

    imports optimized

--- backend/src/main/java/at/teamhasi/hotel/repositories/RoomTypeRepository.java
+++ backend/src/main/java/at/teamhasi/hotel/repositories/RoomTypeRepository.java
@@ -1,14 +1,9 @@
 package at.teamhasi.hotel.repositories;
 
 import at.teamhasi.hotel.entities.RoomTypeEntity;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 @Repository
-public interface RoomTypeRepository extends JpaRepository<RoomTypeEntity, Integer> {}
+public interface RoomTypeRepository extends JpaRepository<RoomTypeEntity, Integer> {
+}
